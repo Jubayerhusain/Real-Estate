b7e5f750527e0045c6ccfc1918fc605998433626
Jubayer husain <jubayerhusai360@gmail.com>
2025-11-24 22:36:47 +0600

    about and contact page added done

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import Client_Layout from "./Layouts/Client_Layout";
 import Home from "./Pages/Home";
-import Property from './Pages/Property';
+import Property from "./Pages/Property";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
 
 const router = createBrowserRouter([
   {
@@ -14,9 +16,21 @@ const router = createBrowserRouter([
       },
       {
         path: "property",
-        element: <Property />
+        element: <Property />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
       }
     ],
+  },
+  {
+    path: "*",
+    element: <div>404 Not Found</div>,
   },
 ]);
 
